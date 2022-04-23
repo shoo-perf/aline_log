4153004ee84b46937d20c1fee739a7a0d38d6bf6
Arkady <arkadiy123440@gmail.com>
2022-04-23 17:56:30 +0300

    pre-prod

--- js/services.js
+++ js/services.js
@@ -1,45 +1,44 @@
- /**Servises list*/
- $(document).ready(function () {
-   const toggles = document.querySelectorAll('.faq-toggle');
-   toggles.forEach(toggle => {
-      toggle.addEventListener('click', () => {
-         toggle.parentNode.classList.toggle('active');
-      });
-   });
-  
- });
-
- /**Header animation*/
+/**Servises list*/
 $(document).ready(function () {
-   let faq_element = $(".faq-title");
-   $(window).scroll(function () {
-     var scroll = $(window).scrollTop() + $(window).height();
-     var offset = faq_element.offset().top;
- 
-     if (scroll > offset) {
+  const toggles = document.querySelectorAll(".faq-toggle");
+  toggles.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+      toggle.parentNode.classList.toggle("active");
+    });
+  });
+});
+
+/**Header animation*/
+$(document).ready(function () {
+  let faq_element = $(".faq-title");
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop() + $(window).height();
+    var offset = faq_element.offset().top;
+
+    if (scroll > offset) {
       faq_element.removeClass("hidden_faq-title");
       faq_element.addClass("animate_faq-title");
-     }
-   });
- });
- 
+    }
+  });
+});
 
- /**Отправка формы*/
-$(document).ready(function() {
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+/**Отправка формы*/
+$(document).ready(function () {
+  //E-mail Ajax Send
+  $("form").submit(function () {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize(),
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
 });
